26a7ea9522d7a0ef728b6107cdea6523d7163257
Tavis Weeden <tavisweeden@gmail.com>
2019-10-01 02:05:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def update_counting_hash(hash, key)
    if hash[key] 
      hash[key] += 1
    else
-     hash[key] = 1
+     hash[key] + 1
      hash
 end
   # given a hash an a key as parameters, return an updated hash
